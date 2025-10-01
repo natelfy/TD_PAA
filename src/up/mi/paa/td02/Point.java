494692d73c3741630c923f2d781aa84968410c53
kubeaux <nathan.eullaffroy@gmail.com>
2025-10-01 12:04:37 +0200

    Fin jour 1

---
+++ src/up/mi/paa/td02/Point.java
@@ -0,0 +1,11 @@
+package up.mi.paa.td02;
+
+public final class Point {
+    private final double abs;
+    private final double ord;
+
+    public Point(double x, double y) {
+        this.abs = x;
+        this.ord = y;
+    }
+}
