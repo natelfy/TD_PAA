4fe6f5cd769fac50536fc440c70f1623c2f0c93b
kubeaux <nathan.eullaffroy@gmail.com>
2025-10-07 16:08:40 +0200

    Exercice 2.1

--- src/up/mi/paa/td02/Point.java
+++ src/up/mi/paa/td02/Point.java
@@ -1,5 +1,7 @@
 package up.mi.paa.td02;
 
+import java.util.Vector;
+
 public final class Point {
     private final double abs;
     private final double ord;
@@ -7,5 +9,11 @@ public final class Point {
     public Point(double x, double y) {
         this.abs = x;
         this.ord = y;
+    }
+    public double getX() { return abs; }
+    public double getY() { return ord; }
+
+    public double distance(Point a) {
+        return Math.hypot(a.abs - this.abs, a.ord - this.ord);
     }
 }
